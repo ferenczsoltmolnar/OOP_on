dc52c9fd1d3a5d1d7d410e8dc03ae2670349c217
molnar76 <molnar76@lab104-15.iit.uni-miskolc.hu>
2018-05-07 16:12:54 +0200

    bbbb

---
+++ Beszamolo/src/teszt/Taroloteszt.java
@@ -0,0 +1,18 @@
+package teszt;
+
+import szgep.Merevlemez;
+
+public class Taroloteszt {
+static String lyon;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (int i = 0;i<5; i++ ){
+			
+			Alkalmazott[] merevlemez;
+			Merevlemez merevlemez1[i] = new Merevlemez("XY" + (i + 1), 1000 + 500
+
+	}
+
+}
